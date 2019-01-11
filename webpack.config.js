3fb6ca395e6a16c48a05ea8d62c9cc3e26a51968
Khoa Pham <kenjifam@gmail.com>
2019-01-11 19:15:49 +0200

    init setup

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: './bundle.js'
+  },
+  devtool:'source-map',
+  resolve: {
+    extensions: ['*','.ts','.tsx','.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.css$/,
+        use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    })
+  ]
+}
